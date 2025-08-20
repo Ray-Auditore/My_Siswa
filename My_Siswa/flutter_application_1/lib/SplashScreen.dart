32f46c78be99688a4d32c377c7ace01a1720ed20
Ray-Auditore <rayyan0116@abudzar.sch.id>
2025-08-20 09:11:29 +0700

    aplikasi siswa

---
+++ My_Siswa/flutter_application_1/lib/SplashScreen.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/animatedSplash');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("Assets/mySiswa.png", width: 200, height: 200),
+            const SizedBox(height: 20),
+            const Text(
+              "MySiswa",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.pink,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ My_Siswa/mysiswa/mysiswa/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:mysiswa/login.dart';
+import 'package:lottie/lottie.dart';
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FlutterSplashScreen.fadeIn(
+        backgroundColor: Colors.white,
+        duration: const Duration(milliseconds: 3000),
+        childWidget: SizedBox(
+          width: 120,
+          height: 120,
+          child: Lottie.asset('image/loading.json'),
+        ),
+        nextScreen: const Login(),
+      ),
+    );
+  }
+}
